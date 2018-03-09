e7e28fa13ebd2ce8cfbff880c90c5a3061953dda
Gyunay Daud <prohause@live.com>
2018-03-09 07:33:04 +0200

    Member1 initial commit

---
+++ intro-and-basic-syntax/exercise/Problem01CreditCardNumber/Problem01CreditCardNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Problem01CreditCardNumber
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var firstGroup = int.Parse(Console.ReadLine());
+            var secondGroup = int.Parse(Console.ReadLine());
+            var thirdGroup = int.Parse(Console.ReadLine());
+            var fourthGroup = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{firstGroup:D4} {secondGroup:D4} {thirdGroup:D4} {fourthGroup:D4}");
+        }
+    }
+}
