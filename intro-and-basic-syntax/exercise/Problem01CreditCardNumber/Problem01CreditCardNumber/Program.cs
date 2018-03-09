f953448310e6672718414cf79a3b91e173b7125b
Ivaylo Nelov Laptop <nelov87@gmail.com>
2018-03-09 16:56:08 +0200

    Document the code by Nelov87

--- intro-and-basic-syntax/exercise/Problem01CreditCardNumber/Problem01CreditCardNumber/Program.cs
+++ intro-and-basic-syntax/exercise/Problem01CreditCardNumber/Problem01CreditCardNumber/Program.cs
@@ -6,11 +6,13 @@ namespace Problem01CreditCardNumber
     {
         private static void Main(string[] args)
         {
+            // We recive groups of numbers
             var firstGroup = int.Parse(Console.ReadLine());
             var secondGroup = int.Parse(Console.ReadLine());
             var thirdGroup = int.Parse(Console.ReadLine());
             var fourthGroup = int.Parse(Console.ReadLine());
 
+            // Print the credit card number
             Console.WriteLine($"{firstGroup:D4} {secondGroup:D4} {thirdGroup:D4} {fourthGroup:D4}");
         }
     }
